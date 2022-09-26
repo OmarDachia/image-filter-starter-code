5a2056d648a5f47f41216e6dc63adb72120ee465
Umar Dachia <umar.dachia@kedco.com>
2022-09-26 13:32:48 +0100

    abbas ango

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   app.get("/filteredimage",async (req:Request,res:Response)=>{
     // console.log("in");
-    const img_url= req.query.image_url;
+    const img_url= req.query.img_url;
     //Validate url
     // const isUrl = img_url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
     if(!img_url){
